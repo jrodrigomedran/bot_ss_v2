102a3e23aee50ff719cc3ddd6d1b978af7e6cc0e
DESKTOP-SNHP3V0\Usuario <jr.rodmed@gmail.com>
2017-08-13 11:29:04 +0200

    Primera versión

--- bot.js
+++ bot.js
@@ -16,17 +16,107 @@
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 var env = require('node-env-file');
-env(__dirname + '/.env');
-
+env(__dirname + '/vars.env');
 
 if (!process.env.PORT) {
-  console.log('Error: Specify studio_token and PORT in environment');
-  usage_tip();
-  process.exit(1);
+    console.log('Error: Specify studio_token and PORT in environment');
+    usage_tip();
+    process.exit(1);
 }
 
 var Botkit = require('botkit');
 var debug = require('debug')('botkit:main');
+
+// Api.ai for BotKit
+const apiaibotkit = require('api-ai-botkit');
+const apiaiToken = process.env.token;
+const apiai = apiaibotkit(apiaiToken);
+// -----------------------------------
+
+// ----------- Mongoose ----------- //
+var mongoose = require('./node_modules/mongoose');
+const mongo_uri = 'mongodb://localhost/SemanaSanta';
+mongoose.connect(mongo_uri, function(error) {
+    if (error) {
+        throw error;
+    } else {
+        let conx = 'Conectado a MongoDB -> ' + mongo_uri;
+        console.log(conx);
+    }
+});
+
+const herm_Schema = new mongoose.Schema({
+    nombre: String,
+    dia: String,
+    salida: String,
+    itinerario: Array
+});
+
+const db_herm = mongoose.model('hermandades', herm_Schema);
+
+var info_hermandades;
+
+// Muestra todo el contenido de la base de datos
+db_herm.find({}, function(err, hermandades) {
+    if (err) {
+        throw err;
+    } else {
+        // console.log(hermandades);
+        info_hermandades = hermandades; // Nos exportamos toda la base de datos
+    }
+});
+
+function encontrarHerm(nombre) {
+    let esta = false;
+    for (let i = 0; i < info_hermandades.length; i++) {
+        if (info_hermandades[i].nombre == nombre) {
+            esta = true;
+        }
+    }
+    return esta;
+}
+
+function buscarDiaProc(nombre) {
+    let dia = "";
+    for (let i = 0; i < info_hermandades.length; i++) {
+        if (info_hermandades[i].nombre == nombre) {
+            dia = info_hermandades[i].dia;
+        }
+    }
+    return dia;
+}
+
+function encontrarHoraSalida(nombre) {
+    let hora = '';
+    for (let i = 0; i < info_hermandades.length; i++) {
+        if (info_hermandades[i].nombre == nombre) {
+            hora = info_hermandades[i].salida;
+        }
+    }
+    return hora;
+}
+
+function buscaProcesiones(dia) {
+    var proc = [];
+    for (let i = 0; i < info_hermandades.length; i++) {
+        if (info_hermandades[i].dia == dia) {
+            proc.push(info_hermandades[i].nombre);
+        }
+    }
+    return proc;
+}
+
+function buscarRecorrido(nombre) {
+    var itinerario = [];
+    for (let i = 0; i < info_hermandades.length; i++) {
+        if (info_hermandades[i].nombre == nombre) {
+            itinerario = info_hermandades[i].itinerario;
+        }
+    }
+    return itinerario;
+}
+
+// -----------------------------------
 
 var bot_options = {
     studio_token: process.env.studio_token,
@@ -36,7 +126,7 @@ var bot_options = {
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
 if (process.env.MONGO_URI) {
-    var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGO_URI});
+    var mongoStorage = require('botkit-storage-mongo')({ mongoUri: process.env.MONGO_URI });
     bot_options.storage = mongoStorage;
 } else {
     bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
@@ -54,58 +144,166 @@ controller.openSocketServer(controller.httpserver);
 // Start the bot brain in motion!!
 controller.startTicking();
 
+controller.hears('.*', ['message_received'], function(bot, message) {
+    apiai.process(message, bot);
+});
+
+controller.on('reaction_added', function(bot, message) {
+    console.log(message);
+});
+
+apiai.all(function(message, resp, bot) {
+    console.log(message);
+    console.log(resp);
+    console.log("Accion: " + resp.result.action);
+    console.log("---------CONTEXTOS---------");
+    if (resp.result.contexts) {
+        for (var i = 0; i < resp.result.contexts.length; i++) {
+            console.log(resp.result.contexts[i]);
+        }
+    }
+});
+
+apiai
+    .action('input.welcome', function(message, resp, bot) {
+        let responseText = resp.result.fulfillment.speech;
+        bot.reply(message, responseText);
+    })
+    .action('input.unknown', function(message, resp, bot) {
+        let responseText = resp.result.fulfillment.speech;
+        bot.reply(message, responseText);
+    })
+    .action('horario', function(message, resp, bot) {
+        let herm = resp.result.contexts[0].parameters.hermandad;
+        if ((herm != "") || (herm != "undefined")) {
+            let respuesta = "Primero tienes que decirme la hermandad por la que preguntas, por ejemplo: " + "<b>pregunto por el horario del Rescatado</b>";
+            bot.reply(message, respuesta);
+        } else {
+            bot.reply(message, "¿Está preguntando por la hora de salida de la hermandad " + herm + "?");
+        }
+    })
+    .action('hermandad', function(message, resp, bot) {
+        let nombre = resp.result.parameters.hermandad;
+        if (encontrarHerm(nombre)) {
+            let respuesta = "Preguntas por la hermandad " + nombre;
+            bot.reply(message, {
+                text: '¿Que deseas saber de la hermandad ' + nombre + '?',
+                attachments: {
+                    quick_replies: [{
+                        text: 'Hora de salida de ' + nombre,
+                        payload: 'Quiero saber la hora de salida de ' + nombre
+                    }, {
+                        text: 'Recorrido',
+                        payload: 'Recorrido'
+                    }]
+                }
+            }, function() {});
+        } else {
+            bot.reply(message, "No existe");
+        }
+    })
+    .action('herm_hora', function(message, resp, bot) {
+        let responseText = resp.result.fulfillment.speech;
+        let nombre = resp.result.parameters.hermandad;
+        if (encontrarHerm(nombre)) {
+            let hora = encontrarHoraSalida(nombre);
+            let cont = "La hermandad " + nombre + ' sale el ' + buscarDiaProc(nombre) + ' a las ' + hora;
+            bot.reply(message, cont);
+            bot.reply(message, {
+                text: '¿Deseas saber más de la hermandad ' + nombre + '?' + ' ¿O quieres volver atrás?',
+                attachments: {
+                    quick_replies: [{
+                        text: 'Quiero saber el recorrido de la hermandad ' + nombre,
+                        payload: 'Quiero saber el recorrido de la hermandad ' + nombre
+                    }, {
+                        text: 'Atrás',
+                        payload: 'Atrás'
+                    }]
+                }
+            }, function() {});
+        } else {
+            bot.reply(message, "No existe");
+        }
+
+    })
+    .action('info_dias', function(message, resp, bot) {
+        let dia_preg = resp.result.parameters.dias;
+        if (dia_preg != "") {
+            let v_proc = [];
+            v_proc = buscaProcesiones(dia_preg);
+            if (v_proc == null) {
+                bot.reply(message, "No hay procesiones");
+            } else {
+                respuesta = "Estás preguntando por el " + dia_preg;
+                bot.reply(message, respuesta);
+                let proc = "";
+                for (let i = 0; i < v_proc.length; i++) {
+                    proc = proc.concat(v_proc[i]);
+                    if (i != (v_proc.length - 1))
+                        proc = proc.concat(", ");
+                }
+                proc = "Las Hermandades que procesionan el " + dia_preg + ": " + proc;
+                bot.reply(message, proc);
+            }
+        } else {
+            bot.reply(message, "Tienes que decirme el día por el que preguntas");
+        }
+    })
+    .action('recorrido', function(message, resp, bot) {
+        let herm = resp.result.contexts[0].parameters.hermandad;
+        if ((herm != "") || (herm != "undefined")) {
+            let respuesta = "Primero tienes que decirme la hermandad por la que preguntas, por ejemplo: " + "<b>pregunto por el recorrido de la merced</b>";
+            bot.reply(message, respuesta);
+        } else {
+            bot.reply(message, "¿Está preguntando por el recorrido de la hermandad " + herm + "?");
+        }
+    })
+    .action('herm_recorrido', function(message, resp, bot) {
+        let responseText = resp.result.fulfillment.speech;
+        let nombre = resp.result.parameters.hermandad;
+        if (encontrarHerm(nombre)) {
+            let respuesta = "Preguntas por el recorrido de la hermandad " + nombre;
+            bot.reply(message, respuesta);
+            let itinerario = [];
+            itinerario = buscarRecorrido(nombre);
+            let recorrido = "";
+            for (let i = 0; i < itinerario.length; i++) {
+                recorrido = recorrido.concat(itinerario[i]);
+                if (i != (itinerario.length - 1))
+                    recorrido = recorrido.concat(", ");
+            }
+            let cont = "La hermandad " + nombre + ' tiene el siguiente recorrido: ' + recorrido;
+            bot.reply(message, cont);
+            bot.reply(message, {
+                text: '¿Deseas saber más de la hermandad ' + nombre + '?' + ' ¿O quieres volver atrás?',
+                attachments: {
+                    quick_replies: [{
+                        text: 'Quiero saber la hora de salida de la hermandad ' + nombre,
+                        payload: 'Quiero saber la hora de salida de la hermandad ' + nombre
+                    }, {
+                        text: 'Atrás',
+                        payload: 'Atrás'
+                    }]
+                }
+            }, function() {});
+        } else {
+            bot.reply(message, "No existe");
+        }
+    })
 
 var normalizedPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./skills/" + file)(controller);
+    require("./skills/" + file)(controller);
 });
 
 console.log('I AM ONLINE! COME TALK TO ME: http://localhost:' + process.env.PORT)
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('message_received', function(bot, message) {
-        controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(function(convo) {
-            if (!convo) {
-                // no trigger was matched
-                // If you want your bot to respond to every message,
-                // define a 'fallback' script in Botkit Studio
-                // and uncomment the line below.
-                // controller.studio.run(bot, 'fallback', message.user, message.channel);
-            } else {
-                // set variables here that are needed for EVERY script
-                // use controller.studio.before('script') to set variables specific to a script
-                convo.setVar('current_time', new Date());
-            }
-        }).catch(function(err) {
-            bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-            debug('Botkit Studio: ', err);
-        });
-    });
-} else {
-
-    controller.on('message_received', function(bot, message) {
-      bot.reply(message, 'I need an API token from Botkit Studio to do more stuff. Get one here: https://studio.botkit.ai')
-    });
-
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
-
-
-
-
 function usage_tip() {
     console.log('~~~~~~~~~~');
-    console.log('Botkit Starter Kit');
+    console.log('BOT para informar acerca de la Semana Santa de Córdoba');
+    console.log('Desarrollado por Jesús Rodrigo');
     console.log('Execute your bot application like this:');
-    console.log('PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
+    console.log('PORT=<PORT> studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
     console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
     console.log('~~~~~~~~~~');
 }
--- skills/connection_events.js
+++ skills/connection_events.js
@@ -1,25 +1,56 @@
 module.exports = function(controller) {
 
-  controller.on('hello', function(bot, message) {
+    controller.on('hello', function(bot, message) {
+        bot.reply(message, {
+            text: '<h2>Bienvenido al bot de la Semana Santa de Córdoba</h2>' +
+                '<b>Puede preguntar por:</b><br><br>' +
+                'Hora de salida de una Hermandad: <b>"hora de salida del Rescatado"</b><br>' +
+                'Itinerario: <b>"recorrido de la Hermandad del Huerto"</b><br>' +
+                '¿Qué procesión ver hoy?: <b>"Hermandades del lunes santo"</b><br>' +
+                'O dejar que te guiemos pulsando en cualquiera de los dos botones: <br>',
+            attachments: {
+                quick_replies: [{
+                    text: 'Hora de salida',
+                    payload: 'Horarios'
+                }, {
+                    text: 'Recorridos',
+                    payload: 'Recorridos'
+                }, {
+                    text: 'Procesiones',
+                    payload: 'Procesiones'
+                }]
+            }
+        }, function() {});
+    });
 
-    // a new session with an unknown user has begun
-    bot.reply(message, 'Hello! I am a Botkit bot! Please configure me to do something useful.');
+    controller.on('welcome_back', function(bot, message) {
+        bot.reply(message, {
+            text: '<h2>Bienvenido al bot de la Semana Santa de Córdoba</h2>' +
+                '<b>Puede preguntar por:</b><br><br>' +
+                'Hora de salida de una Hermandad: <b>"hora de salida del Rescatado"</b><br>' +
+                'Itinerario: <b>"recorrido de la Hermandad del Huerto"</b><br>' +
+                '¿Qué procesión ver hoy?: <b>"Hermandades del lunes santo"</b><br>' +
+                'O dejar que te guiemos pulsando en cualquiera de los dos botones: <br>',
+            attachments: {
+                quick_replies: [{
+                    text: 'Hora de salida',
+                    payload: 'Horarios'
+                }, {
+                    text: 'Recorridos',
+                    payload: 'Recorridos'
+                }, {
+                    text: 'Procesiones',
+                    payload: 'Procesiones'
+                }]
+            }
+        }, function() {});
+    });
 
-  });
+    controller.on('reconnect', function(bot, message) {
+        // the connection between the client and server experienced a disconnect/reconnect
+        bot.reply(message, 'Ha surgido un error, pero ya está solucionado, disculpe las molestias');
 
-  controller.on('welcome_back', function(bot, message) {
-
-    // a known user has started a new, fresh session
-    bot.reply(message, 'Hello again. I still need to be configured.');
-
-  });
-
-  controller.on('reconnect', function(bot, message) {
-
-    // the connection between the client and server experienced a disconnect/reconnect
-    bot.reply(message, 'Some sub-space interference just caused our connection to be interrupted. But I am back now.');
-
-  });
+    });
 
 
 }
